d38d1f680c210d6379651fc245692fcf7b8fe951
Lisgevan <episimo@hotmail.com>
2025-03-06 18:56:40 +0200

    local google login works

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
-// app/auth.js
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import Users from "./models/Users";
+import connectToDatabase from "@/lib/mongodb";
 
 const { handlers, auth, signIn, signOut } = NextAuth({
 	providers: [
@@ -14,6 +15,23 @@ const { handlers, auth, signIn, signOut } = NextAuth({
 			},
 		}),
 	],
+	callbacks: {
+		async signIn({ profile }) {
+			await connectToDatabase();
+			console.log("Searching for user with email:", profile.email);
+			const user = await Users.findOne({ email: profile.email });
+			console.log("User found:", user);
+			if (user) {
+				return {
+					id: profile.sub,
+					name: profile.name,
+					email: profile.email,
+				};
+			} else {
+				return null;
+			}
+		},
+	},
 	secret: process.env.NEXTAUTH_SECRET,
 });
 
--- middleware.js
+++ middleware.js
@@ -1,1 +1,27 @@
-export { auth as middleware } from "@/auth";
+// export { auth as middleware } from "@/auth";
+
+// app/middleware.js
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+	const session = await auth();
+	const pathname = req.nextUrl.pathname;
+
+	// Allow access to the homepage (/) regardless of authentication status
+	if (pathname === "/") {
+		return NextResponse.next();
+	}
+
+	// If not logged in, redirect to the homepage
+	if (!session) {
+		return NextResponse.redirect(new URL("/", req.url));
+	}
+
+	// User is logged in, allow access to other routes
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
